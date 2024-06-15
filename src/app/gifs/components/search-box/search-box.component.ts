e33119eb091130d3c66c663218a9ad9e200485d3
OrlandoYV <sergioyv@gmail.com>
2024-06-15 13:01:21 -0500

    oy - gifs service

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,6 @@
+import { GifsModule } from './../../gifs.module';
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -9,10 +11,13 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput')
   public tagInput!: ElementRef<HTMLInputElement>;
 
-  constructor() {}
+  constructor(private gifsService: GifsService) {}
 
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
     console.log({ newTag });
+    this.gifsService.searchTag(newTag);
+    this.tagInput.nativeElement.value = "";
+
   }
 }
