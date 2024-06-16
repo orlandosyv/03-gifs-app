d784c530a541c0da0e6a7e8b1045a23d117f477e
OrlandoYV <sergioyv@gmail.com>
2024-06-16 16:00:46 -0500

    oy - gifs service and api key

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({providedIn: 'root'})
-export class GifsService
-{
+@Injectable({ providedIn: 'root' })
+export class GifsService {
   private _tagHistory: string[] = [];
-  constructor() { }
+  private ApiKey: string = 'XNzDUQeDAyG7og8Yf7N61n5ypRV0vkhX';
+
+  constructor() {}
 
   get tagsHistory() {
     return this._tagHistory;
   }
 
-  public searchTag(tag: string): void
-  {
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if (this._tagHistory.includes(tag)) {
+      this._tagHistory = this._tagHistory.filter((oldTag) => oldTag !== tag);
+    }
+
     this._tagHistory.unshift(tag);
+    this._tagHistory = this._tagHistory.splice(0, 10);
+  }
+
+  public searchTag(tag: string): void {
+    if (tag.length === 0) return; //validation of not empty
+    if (/^\d+$/.test(tag)) {
+      // Do nothing if the tag contains only numbers
+      return;
+    }
+    this.organizeHistory(tag);
   }
 }
