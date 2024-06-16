4f8fbc3d40ac9c0e4702f20c67f0b0c5448ab324
OrlandoYV <sergioyv@gmail.com>
2024-06-16 16:23:58 -0500

    oy - get from api

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,13 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
   private _tagHistory: string[] = [];
   private ApiKey: string = 'XNzDUQeDAyG7og8Yf7N61n5ypRV0vkhX';
+  private ApiURL: string = 'https://api.giphy.com/v1/gifs'
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return this._tagHistory;
@@ -29,5 +31,18 @@ export class GifsService {
       return;
     }
     this.organizeHistory(tag);
+
+    const params = new HttpParams().set('api_key', this.ApiKey)
+      .set('limit', 10)
+      .set('q', tag)
+
+    this.http.get(`${this.ApiURL}/search`, {params}).subscribe(resp => {console.log(resp)});
+
+
+
+    // fetch(
+    //   )
+    //   .then(resp => resp.json())
+    //   .then(data => console.log(data));
   }
 }
