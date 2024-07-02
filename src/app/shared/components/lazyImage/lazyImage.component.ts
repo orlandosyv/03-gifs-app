bbc02a73b9e70e11a469ca7b67d6eb191b746a4b
OrlandoYV <sergioyv@gmail.com>
2024-07-02 18:45:13 -0500

    oy - loader svg

--- src/app/shared/components/lazyImage/lazyImage.component.ts
+++ src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -1,8 +1,25 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazyImage.component.html',
 })
-export class LazyImageComponent { }
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = "";
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('Url property is required!');
+  }
+
+  onLoad() {
+    console.log('Image loaded');
+    this.hasLoaded = true;
+  }
+}
