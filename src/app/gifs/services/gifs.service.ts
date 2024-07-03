74f92089a2b90fbe3cf82bb47e01a6386daeeb0f
OrlandoYV <sergioyv@gmail.com>
2024-07-03 13:38:00 -0500

    oy - fixes on searchTag

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,16 +4,15 @@ import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
-
   public gifsList: Gif[] = [];
   private _tagHistory: string[] = [];
   private ApiKey: string = 'XNzDUQeDAyG7og8Yf7N61n5ypRV0vkhX';
-  private ApiURL: string = 'https://api.giphy.com/v1/gifs'
+  private ApiURL: string = 'https://api.giphy.com/v1/gifs';
 
   constructor(private http: HttpClient) {
     this.loadLocalStorage();
-    this.searchTag(this.tagsHistory[0])
-    console.log('service ready!')
+    this.searchTag(this.tagsHistory[0]);
+    console.log('service ready!');
   }
 
   get tagsHistory() {
@@ -38,29 +37,19 @@ export class GifsService {
   }
 
   private saveLocalStorage() {
-    localStorage.setItem('history', JSON.stringify(this._tagHistory))
+    localStorage.setItem('history', JSON.stringify(this._tagHistory));
   }
 
   public searchTag(tag: string): void {
-    if (tag.length === 0) return; //validation of not empty
+    if (!tag || tag.length === 0) return; // Check if tag is not undefined, null, or empty
+
     if (/^\d+$/.test(tag)) {
       // Do nothing if the tag contains only numbers
       return;
     }
+
     this.organizeHistory(tag);
 
-    const params = new HttpParams().set('api_key', this.ApiKey)
-      .set('limit', 12)
-      .set('q', tag)
-
-    this.http.get<SearchResponse>(`${this.ApiURL}/search`, { params })
-      .subscribe((resp) => {
-        this.gifsList = resp.data;
-        console.log({gifs: this.gifsList})
-      });
-  }
-
-  public searchFromHistory(tag: string): void{
     const params = new HttpParams()
       .set('api_key', this.ApiKey)
       .set('limit', 12)
@@ -74,4 +63,17 @@ export class GifsService {
       });
   }
 
+  public searchFromHistory(tag: string): void {
+    const params = new HttpParams()
+      .set('api_key', this.ApiKey)
+      .set('limit', 12)
+      .set('q', tag);
+
+    this.http
+      .get<SearchResponse>(`${this.ApiURL}/search`, { params })
+      .subscribe((resp) => {
+        this.gifsList = resp.data;
+        console.log({ gifs: this.gifsList });
+      });
+  }
 }
